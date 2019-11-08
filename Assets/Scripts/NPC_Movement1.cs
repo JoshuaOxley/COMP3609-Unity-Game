c313b7da9122291db18fda82865131ccbc55dd2a
JoshuaOxley <jmjoxley@gmail.com>
2019-11-08 18:22:30 -0400

    initial
    class GP lab

---
+++ Assets/Scripts/NPC_Movement1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Movement1 : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+
+    float speed = 1.5f;
+    int direction = 1;
+    float timer;
+    float movementTime = 2.0f;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = movementTime;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = rigidbody2D.position;
+        timer -= Time.deltaTime;
+
+        if(timer < 0)
+        {
+            direction =  -direction;
+            timer = movementTime;
+            animator.SetFloat("Move Y", direction);
+        }
+
+        position.y = position.y + speed * direction * Time.deltaTime;
+        rigidbody2D.MovePosition(position);
+    }
+}
---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+    float speed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 30;
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        //if Ruby is moving
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            //set her look direction
+            lookDirection.Set(move.x, move.y);
+            //Normalize the Vector2 so that the x and y values are either 0, 1 or -1
+            lookDirection.Normalize();
+        }
+
+        //set the float values for the animation
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        Vector2 position = rigidbody2D.position;  
+
+        position = position + speed * move * Time.deltaTime;
+
+        /* 
+        Vector2 position = transform.position;
+        position.x = position.x + 3f * horizontal * Time.deltaTime;
+        position.y = position.y + 3f * vertical * Time.deltaTime;
+        transform.position = position;  
+        */
+
+        rigidbody2D.MovePosition(position);
+    }
+}
